b3ca5e8fd33c68018b7d67a34a3984716315b84a
Egerton <tgfc073@tgfc.tw>
2020-03-26 14:19:29 +0800

    Initial commit

---
+++ src/main/java/com/tgfc/tw/amsexample/AmsExampleApplication.java
@@ -0,0 +1,13 @@
+package com.tgfc.tw.amsexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmsExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AmsExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tgfc/tw/amsexample/AmsExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tgfc.tw.amsexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmsExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
